4e682e2cce14a1d9b08b04b6a3ef9d7d6663c3ba
Neuromancer93 <itrejonumber1@gmail.com>
2019-07-19 10:08:07 -0700

    added fab button

--- src/Components/Exercises/Dialogs/Create.js
+++ src/Components/Exercises/Dialogs/Create.js
@@ -1,3 +1,32 @@
-import React from 'react';
+import React, { Fragment, Component } from 'react';
+import { Dialog, Button, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
-export default (props) => <div>Hello from this div</div>;
+export default class extends Component {
+	state = {
+		open: false
+	};
+
+	render() {
+		const { open } = this.state;
+
+		return (
+			<Fragment>
+				<Fab color="secondary" size="small">
+					<AddIcon />
+				</Fab>
+				<Dialog open={open} aria-labelledby="form-dialog-title">
+					<DialogTitle id="form-dialog-title">Create a new exercise</DialogTitle>
+					<DialogContent>
+						<DialogContentText>Please fill out the form below</DialogContentText>
+						<form />
+					</DialogContent>
+					<DialogActions>
+						<Button color="primary">Create</Button>
+					</DialogActions>
+				</Dialog>
+			</Fragment>
+		);
+	}
+}
--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import CreateDialog from '../Exercises/Dialogs/Create';
 
 export default (props) => (
 	<AppBar position="static">
 		<Toolbar>
-			<Typography variant="headline" color="inherit">
+			<Typography variant="headline" color="inherit" style={{ flex: 1 }}>
 				Exercise Database
 			</Typography>
+
+			<CreateDialog />
 		</Toolbar>
 	</AppBar>
 );
